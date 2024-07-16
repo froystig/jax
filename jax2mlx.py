e0f843eaa5361b79e734162ecdd6a5004feaa137
Roy Frostig <frostig@google.com>
2024-07-16 15:37:52 -0700

    jit/transformation-like API and nested pytree support

--- jax2mlx.py
+++ jax2mlx.py
@@ -1,19 +1,23 @@
 from __future__ import annotations
 
-from functools import partial
-
-import mlx
-import mlx.core as mx
+from functools import partial, wraps
 
 import jax
 import jax.numpy as jnp
-
 import jax.extend as jex
+import mlx
+import mlx.core as mx
 
+from jax._src import api_util
+from jax._src import linear_util
 from jax._src import util
 map, unsafe_map = util.safe_map, map
 zip, unsafe_zip = util.safe_zip, zip
 
+
+_dtype_mappings = {
+  mx.float32: jnp.dtype('float32'),
+}
 
 _mlx_rules = {}
 
@@ -22,6 +26,21 @@ def get_mlx_rule(p: jex.core.Primitive):
 
 _mlx_rules[jex.core.primitives.add_p] = mx.add
 _mlx_rules[jex.core.primitives.cos_p] = mx.cos
+
+
+def abstractify_mlx_value(x):
+  return jax.ShapeDtypeStruct(x.shape, _dtype_mappings.get(x.dtype, x.dtype))
+
+def jax2mlx(f):
+  @wraps(f)
+  def mlxed(*args, **kwargs):
+    args_flat, in_tree = jax.tree.flatten((args, kwargs))
+    flat_fun, out_tree = api_util.flatten_fun(linear_util.wrap_init(f), in_tree)
+    tys_flat = map(abstractify_mlx_value, args_flat)
+    jaxpr = jax.make_jaxpr(flat_fun.call_wrapped)(*tys_flat)
+    outs_flat = mlx_eval_jaxpr(jaxpr, *args_flat)
+    return jax.tree.unflatten(out_tree(), outs_flat)
+  return mlxed
 
 
 def mlx_eval_jaxpr(closed_jaxpr, *args):
@@ -47,16 +66,21 @@ def mlx_eval_jaxpr(closed_jaxpr, *args):
   return map(read, jaxpr.outvars)
 
 
+def test_f(x, y):
+  return x + jax.lax.cos(y)
+
 def main(*args):
   ins_jax = [jnp.array(float(x)) for x in args]
   ins_mlx = [mx.array(float(x)) for x in args]
-  jaxpr = jax.make_jaxpr(lambda x, y: x + jax.lax.cos(y))(*ins_jax)
-  jax_eval = jex.core.jaxpr_as_fun(jaxpr)
+  f = test_f
+
+  jax_eval = f
   jax_ceval = jax.jit(jax_eval)
-  mlx_eval = partial(mlx_eval_jaxpr, jaxpr)
-  mlx_ceval = mx.compile(mlx_eval)
   print('jax eag:', jax_eval(*ins_jax))
   print('jax jit:', jax_ceval(*ins_jax))
+
+  mlx_eval = jax2mlx(f)
+  mlx_ceval = mx.compile(mlx_eval)
   print('mlx eag:', mlx_eval(*ins_mlx))
   print('mlx jit:', mlx_ceval(*ins_mlx))
 
